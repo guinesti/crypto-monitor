908a248d8375e6a02d5c0e3f24dffffebae035b4
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-07 20:55:30 -0300

    first commit

---
+++ app/src/main/java/guinesti/com/github/crypto_monitor/service/MercadoBitcoinService.kt
@@ -0,0 +1,10 @@
+package guinesti.com.github.crypto_monitor.service
+
+import guinesti.com.github.crypto_monitor.model.TickerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MercadoBitcoinService {
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
